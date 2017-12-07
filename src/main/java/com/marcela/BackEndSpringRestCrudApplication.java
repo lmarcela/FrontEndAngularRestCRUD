dfdc3cf7e7a88e56c46075f150cb960f9ae12891
Marcela Malaver <marcela9409@gmail.com>
2017-12-07 16:38:51 -0500

    Project created

---
+++ src/main/java/com/marcela/BackEndSpringRestCrudApplication.java
@@ -0,0 +1,12 @@
+package com.marcela;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndSpringRestCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEndSpringRestCrudApplication.class, args);
+	}
+}
