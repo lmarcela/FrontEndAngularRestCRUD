ef418e57229b217848fc18420c48e623ad47a958
Marcela Malaver <marcela9409@gmail.com>
2017-12-11 12:07:33 -0500

    Angular: Service created and configured

--- src/main/resources/FrontEndAngularRestCRUD/src/app/app.module.ts
+++ src/main/resources/FrontEndAngularRestCRUD/src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
+import { HttpModule } from '@angular/http'
 
 import { AppComponent } from './app.component';
 import { ListuserComponent } from './components/listuser/listuser.component';
 import { UserFormComponent } from './components/user-form/user-form.component';
+import { UserService } from './services/user.service'
 
 const appRoutes:Routes=[
   {path:'',component:ListuserComponent},
@@ -20,9 +21,10 @@ const appRoutes:Routes=[
   ],
   imports: [
     BrowserModule,
+    HttpModule,
     RouterModule.forRoot(appRoutes)
   ],
-  providers: [],
+  providers: [UserService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
